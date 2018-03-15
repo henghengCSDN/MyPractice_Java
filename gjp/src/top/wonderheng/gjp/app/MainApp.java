0656c2aa868a1501e35616216e8872d4b3251016
wonderheng <995107866@qq.com>
2018-03-16 00:29:01 +0800

    一个简单的家庭记账小程序

---
+++ gjp/src/top/wonderheng/gjp/app/MainApp.java
@@ -0,0 +1,15 @@
+package top.wonderheng.gjp.app;
+
+import top.wonderheng.gjp.view.MainView;
+
+/*
+ * 主程序类，开启软件程序
+ */
+public class MainApp {
+
+	public static void main(String[] args) {
+		//view层
+		new MainView().run();
+	}
+
+}
